b701ba03cdcfb894332eea740678b8cd5ba246ed
Ross Elliman <rosselliman@hotmail.com>
2024-12-19 20:20:28 +0000

    Disable Debug

--- food_ordering_system/settings.py
+++ food_ordering_system/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-russlyman-foodorderings-e1g3n5z55mu.ws.codeinstitute-ide.net', 'rje-food-ordering-system-e7aa385812ff.herokuapp.com', 'localhost']
 
