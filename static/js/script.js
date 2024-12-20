aff83b10d8e81de594cd63d1eb955699df20d204
Ross Elliman <rosselliman@hotmail.com>
2024-12-20 14:08:19 +0000

    Hide description from modal if these isn't one

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,6 @@
 // Custom JS Here
 const exampleModal = document.getElementById('itemInfoModal')
+const exampleModalBody = document.getElementById('itemInfoModalBody')
 
 if (exampleModal) {
   exampleModal.addEventListener('show.bs.modal', event => {
@@ -11,6 +12,14 @@ if (exampleModal) {
     const modalTitle = exampleModal.querySelector('.modal-title')
     const modalPrice = exampleModal.querySelector('.modal-price')
     const modalDescription = exampleModal.querySelector('.modal-description')
+    const modalDescriptionWrapper = exampleModal.querySelector('.modal-description-wrapper')
+
+    // Hide description if these is none
+    if (menuItemDescription) {
+      modalDescriptionWrapper.classList.remove("d-none")
+    } else {
+      modalDescriptionWrapper.classList.add("d-none")
+    }
 
     modalTitle.innerText = menuItemName
     modalPrice.innerText = menuItemPrice
