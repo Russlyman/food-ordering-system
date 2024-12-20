cb7e7ed8c3c1bf7f49965d1f728455ebcc09bfb2
Ross Elliman <rosselliman@hotmail.com>
2024-12-20 11:37:24 +0000

    Display categories and items form database in the template

--- menu/views.py
+++ menu/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.db.models import Count
+from .models import Category
 
 # Create your views here.
-class HomePage(TemplateView):
-    """
-    Displays home page"
-    """
-    template_name = 'menu/menu.html'
+def menu_list(request):
+    # Gets all categories that have items. gt = greater than
+    categories = Category.objects.annotate(num_items=Count('items')).filter(num_items__gt=0)
+
+    return render(
+        request,
+        "menu/menu_list.html",
+        {"categories": categories},
+    )
