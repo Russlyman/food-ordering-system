f7164c9a31f2f8087415bda32748f4f5769efef6
Ross Elliman <rosselliman@hotmail.com>
2024-12-20 10:20:02 +0000

    Add Category and Item models

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Item(models.Model):
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="items"
+    )
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    image = CloudinaryField("image", default="placeholder")
+    price = models.DecimalField(decimal_places=2)
