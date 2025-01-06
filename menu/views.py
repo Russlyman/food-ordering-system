b73d58403746eea5aecdd57900cc78ebae81c688
Ross Elliman <rosselliman@hotmail.com>
2025-01-06 22:38:20 +0000

    Update existing ItemOrder quantities instead of making new objec

--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, reverse
 from django.db.models import Count
-from .models import Category, Item, Order
+from .models import Category, Item, Order, ItemOrder
 from .forms import QuantityForm
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404
@@ -21,17 +21,36 @@ def basket_quantity(request, item_id):
         return HttpResponse(status=405)
     
     order = Order.objects.get(id=1)
-
     item = get_object_or_404(Item, id=item_id)
-    quantity_form = QuantityForm(data=request.POST)
 
+    quantity_form = QuantityForm(data=request.POST)
     if not quantity_form.is_valid():
         return HttpResponse(status=400)
-    
-    itemorder = quantity_form.save(commit=False)
-    itemorder.item = item
-    itemorder.order = order
 
-    itemorder.save()
+    try:
+        itemorder = ItemOrder.objects.get(order=order, item=item)
+
+        itemorder.quantity = quantity_form.cleaned_data['quantity']
+        
+        itemorder.save()
+    except ItemOrder.DoesNotExist:
+        itemorder = quantity_form.save(commit=False)
+        
+        itemorder.item = item
+        itemorder.order = order
+
+        itemorder.save()
+
+    # item = get_object_or_404(Item, id=item_id)
+    # quantity_form = QuantityForm(data=request.POST)
+
+    # if not quantity_form.is_valid():
+    #     return HttpResponse(status=400)
+    
+    # itemorder = quantity_form.save(commit=False)
+    # itemorder.item = item
+    # itemorder.order = order
+
+    # itemorder.save()
 
     return HttpResponseRedirect(reverse('home'))
