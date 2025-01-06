6f64a9781029164f8b670ece552e5d94ba1a9fa4
Ross Elliman <rosselliman@hotmail.com>
2025-01-06 19:05:33 +0000

    Add Order and ItemOrder models

--- menu/admin.py
+++ menu/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Category, Item
+from .models import Category, Item, Order, ItemOrder
 
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Item)
+admin.site.register(Order)
+admin.site.register(ItemOrder)
--- menu/models.py
+++ menu/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+ORDER_TYPE = ((0, "Collection"), (1, "Delivery"))
 
 
 class Category(models.Model):
@@ -9,7 +12,7 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = "Categories"
-    
+
     def __str__(self):
         return self.name
 
@@ -25,3 +28,29 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    placed = models.BooleanField(default=False)
+    completed = models.BooleanField(default=False)
+    order_type = models.IntegerField(choices=ORDER_TYPE, null=True, blank=False)
+
+    def __str__(self):
+        return f"Order {self.id} - {self.user.username}"
+
+
+class ItemOrder(models.Model):
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name="items"
+    )
+    item = models.ForeignKey(
+        Item, on_delete=models.CASCADE, related_name="orders"
+    )
+    quantity = models.IntegerField(default=1)
+
+    class Meta:
+        verbose_name_plural = "ItemOrders"
+
+    def __str__(self):
+        return f"Order {self.order.id} - {self.order.user.username} - {self.item.name}"
