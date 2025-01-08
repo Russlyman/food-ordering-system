cf2453efe84323373a29e9474b13cb34efd9afd4
Ross Elliman <rosselliman@hotmail.com>
2025-01-08 12:10:11 +0000

    Add some comments before we forget

--- menu/views.py
+++ menu/views.py
@@ -20,19 +20,24 @@ def basket_quantity(request, item_id):
     if request.method != "POST":
         return HttpResponse(status=405)
     
+    # FIX: Active order should not be hard coded and should be unique for each user.
+    # Get item from database.
     order = Order.objects.get(id=1)
     item = get_object_or_404(Item, id=item_id)
 
+    # Kill invalid forms
     quantity_form = QuantityForm(data=request.POST)
     if not quantity_form.is_valid():
         return HttpResponse(status=400)
 
+    # Update existing basket quantity if item is already in basket.
     try:
         itemorder = ItemOrder.objects.get(order=order, item=item)
 
         itemorder.quantity = quantity_form.cleaned_data['quantity']
         
         itemorder.save()
+    # Add new basket item to the db.
     except ItemOrder.DoesNotExist:
         itemorder = quantity_form.save(commit=False)
         
@@ -40,17 +45,5 @@ def basket_quantity(request, item_id):
         itemorder.order = order
 
         itemorder.save()
-
-    # item = get_object_or_404(Item, id=item_id)
-    # quantity_form = QuantityForm(data=request.POST)
-
-    # if not quantity_form.is_valid():
-    #     return HttpResponse(status=400)
-    
-    # itemorder = quantity_form.save(commit=False)
-    # itemorder.item = item
-    # itemorder.order = order
-
-    # itemorder.save()
 
     return HttpResponseRedirect(reverse('home'))
--- static/js/script.js
+++ static/js/script.js
@@ -4,24 +4,30 @@ document.addEventListener('DOMContentLoaded', function () {
   const exampleModalBody = document.getElementById('itemInfoModalBody');
   const quantityForm = document.getElementById('quantityForm');
 
-  const modalTotal = document.querySelector('.modal-total');
   let menuItemPrice = 0;
+  const modalTotal = document.querySelector('.modal-total');
   const modalQuantity = document.querySelector('.modal-quantity');
 
+  // Calculates item total after considering quantity.
   function calcTotal() {
+    // Bring quantity back into acceptable range.
     if (modalQuantity.value < 1) {
       modalQuantity.value = 1;
     } else if (modalQuantity.value > 10) {
       modalQuantity.value = 10;
     }
 
+    // 2dp please :)
     modalTotal.innerText = (menuItemPrice * modalQuantity.value).toFixed(2);
   }
 
+  // Recalculate item total when user changes quantity.
   modalQuantity.addEventListener('change', calcTotal);
 
   if (exampleModal) {
+    // When the modal is opened do this...
     exampleModal.addEventListener('show.bs.modal', event => {
+      // Getting attributes from the item that triggered the modal.
       const menuItem = event.relatedTarget;
       const menuItemName = menuItem.getAttribute('data-item-name');
       menuItemPrice = menuItem.getAttribute('data-item-price');
@@ -31,29 +37,37 @@ document.addEventListener('DOMContentLoaded', function () {
       const menuItemQuantity = menuItem.getAttribute('data-item-quantity');
       const menuItemId = menuItem.getAttribute('data-item-id');
 
+      // Getting the elements on the modal.
       const modalTitle = exampleModal.querySelector('.modal-title');
       const modalPrice = exampleModal.querySelector('.modal-price');
-
       const modalDescription = exampleModal.querySelector('.modal-description');
       const modalDescriptionWrapper = exampleModal.querySelector(
         '.modal-description-wrapper'
       );
 
-      // Hide description if these is none
+      // Hide item description if these is none.
       if (menuItemDescription) {
         modalDescriptionWrapper.classList.remove('d-none');
       } else {
         modalDescriptionWrapper.classList.add('d-none');
       }
 
+      // Set item details on the modal.
       modalTitle.innerText = menuItemName;
       modalPrice.innerText = menuItemPrice;
       modalDescription.innerText = menuItemDescription;
+
+      // Set item quantity.
+      // 0 quantity means user hasn't added item to basket, change to 1 as they anticipate adding this item.
       modalQuantity.value =
         menuItemQuantity === '0'
-          ? Number(menuItemQuantity + 1)
+          ? '1'
           : menuItemQuantity;
+      
+      // Update the total cost of an item after considering quantity.
       calcTotal();
+
+      // Change form action to item specific value.
       quantityForm.setAttribute('action', `basket_quantity/${menuItemId}`);
     });
   }
@@ -61,17 +75,22 @@ document.addEventListener('DOMContentLoaded', function () {
   const allItems = document.querySelectorAll('.menu-item');
   const allCategories = document.querySelectorAll('.menu-category');
   const searchBox = document.querySelector('.search-box');
+
+  // Run the search function when stuff is entered in search box.
   searchBox.addEventListener('input', searchItems);
 
   function searchItems(e) {
+    // Normalise search query.
     const searchQuery = e.target.value.toLowerCase().replaceAll(' ', '');
 
     for (let item of allItems) {
+      // Normalise item name.
       const searchItemName = item
         .getAttribute('data-item-name')
         .toLowerCase()
         .replaceAll(' ', '');
 
+      // Hide of show item if it matches search criteria.
       if (searchItemName.includes(searchQuery)) {
         item.parentElement.classList.remove('d-none');
       } else {
@@ -80,6 +99,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     for (let category of allCategories) {
+      // Count items that are showing in this category.
       let showingItems = 0;
       for (let item of category.children) {
         if (!item.classList.contains("d-none")) {
@@ -87,6 +107,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
       }
 
+      // Hide or show this category depending on if it has items.
       if (showingItems === 0) {
         category.parentElement.classList.add('d-none')
       } else {
