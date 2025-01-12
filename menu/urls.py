7eb51071e082d8e0c7f0942cd68157e8b82c82e6
Ross Elliman <rosselliman@hotmail.com>
2025-01-12 16:40:33 +0000

    Add basket page

--- menu/urls.py
+++ menu/urls.py
@@ -2,8 +2,12 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', 
-        views.menu_list, name='home'),
-    path('basket_quantity/<int:item_id>', 
-        views.basket_quantity, name='basket_quantity'),
+    path("", views.menu_list, name="home"),
+    path(
+        "basket_quantity/<int:item_id>", views.basket_quantity, name="basket_quantity"
+    ),
+    path(
+        "delete_item/<int:item_id>", views.delete_item, name="delete_item"
+    ),
+    path("basket", views.basket, name="basket"),
 ]
--- menu/views.py
+++ menu/views.py
@@ -5,13 +5,22 @@ from .forms import QuantityForm
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404
 
+
 # Create your views here.
 def menu_list(request):
     # Gets all categories that have items. gt = greater than
-    categories = Category.objects.annotate(num_items=Count('items')).filter(num_items__gt=0).prefetch_related('items')
+    categories = (
+        Category.objects.annotate(num_items=Count("items"))
+        .filter(num_items__gt=0)
+        .prefetch_related("items")
+    )
 
     # Fetch the current order (assuming ID=1 for simplicity)
-    current_order = Order.objects.prefetch_related('items__item').filter(id=1, user=request.user).first()
+    current_order = (
+        Order.objects.prefetch_related("items__item")
+        .filter(id=1, user=request.user)
+        .first()
+    )
 
     # Build a dictionary of item quantities
     item_quantities = {}
@@ -32,13 +41,19 @@ def menu_list(request):
     return render(
         request,
         "menu/menu_list.html",
-        {"categories": categories, "item_quantities": item_quantities, "currency_symbol": "£", "basket_count": basket_count,},
+        {
+            "categories": categories,
+            "item_quantities": item_quantities,
+            "currency_symbol": "£",
+            "basket_count": basket_count,
+        },
     )
+
 
 def basket_quantity(request, item_id):
     if request.method != "POST":
         return HttpResponse(status=405)
-    
+
     # FIX: Active order should not be hard coded and should be unique for each user.
     # Get item from database.
     order = Order.objects.get(id=1)
@@ -53,18 +68,18 @@ def basket_quantity(request, item_id):
     try:
         itemorder = ItemOrder.objects.get(order=order, item=item)
 
-        itemorder.quantity = quantity_form.cleaned_data['quantity']
+        itemorder.quantity = quantity_form.cleaned_data["quantity"]
 
         # Remove items that are set to a quantity of zero.
         if itemorder.quantity == 0:
             itemorder.delete()
-            return HttpResponseRedirect(reverse('home'))
+            return HttpResponseRedirect(reverse("home"))
 
         itemorder.save()
     # Add new basket item to the db.
     except ItemOrder.DoesNotExist:
         itemorder = quantity_form.save(commit=False)
-        
+
         itemorder.item = item
         itemorder.order = order
 
@@ -74,4 +89,27 @@ def basket_quantity(request, item_id):
 
         itemorder.save()
 
-    return HttpResponseRedirect(reverse('home'))
+    return HttpResponseRedirect(reverse("home"))
+
+
+def basket(request):
+    item_orders = ItemOrder.objects.filter(order_id=1).select_related("item")
+    items = [
+        {
+            "name": item_order.item.name,
+            "id": item_order.item.id,
+            "quantity": item_order.quantity,
+            "price": item_order.item.price
+            * item_order.quantity,  # Calculate total price
+        }
+        for item_order in item_orders
+    ]
+
+    total = sum(item["price"] for item in items)
+
+    return render(request, "menu/basket.html", {"items": items, "total": total, "currency_symbol": "£",})
+
+def delete_item(request, item_id):
+    item = get_object_or_404(ItemOrder, order__id=1, item__id=item_id)
+    item.delete()
+    return HttpResponseRedirect(reverse("basket"))
